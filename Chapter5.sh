d9c0ea41b6cc4f05e5c16d28c2081cabd25591b1
Glanel Houenouvi <grenack@gmail.com>
2018-02-13 23:45:28 -0600

    Week5

---
+++ Chapter5.sh
@@ -0,0 +1,13 @@
+#Chapter 5 Command 
+#i want to change the directory to the boot
+cd /boot
+#show what is in the  directory 
+ls
+#change the directory to grub
+cd grub
+#show all in the grub
+ls
+#i want to show the contain of the grub in less
+less grub.cfg
+
+
